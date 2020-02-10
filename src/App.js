abf6a75f1c0760f2dcf83440055c28dc9179265a
mmg0311 <mohakgadge@gmail.com>
2020-02-10 17:18:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import List from './js/components/List';
+import Form from './js/components/Form';
+import { TextButton, ButtonGroup } from '@dailykit/ui'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <ButtonGroup align='left'>
+         <TextButton type="solid">Solid Button</TextButton>
+      </ButtonGroup>
+        <div>
+          <h2>Articles</h2>
+          <List />
+        </div>
+        <div>
+          <h2>Add a new article</h2>
+          <Form />
+        </div>
+
       </header>
     </div>
   );
